e98f97360213ba95f0903d26c2ac30bbc32f212e
Thomas Ottosson <tomasottosson1@gmail.com>
2025-05-03 00:27:33 +0200

    updated context processor

--- bag/views.py
+++ bag/views.py
@@ -1,25 +1,71 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Product, Category
 
 # Create your views here.
 
-def view_bag(request):
-    """ A view that renders the bag contents page """
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
 
-    return render(request, 'bag/bag.html')
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+    }
+
+    return render(request, 'products/products.html', context)
 
 
-def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+def product_detail(request, product_id):
+    """ A view to show individual product details """
 
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+    product = get_object_or_404(Product, pk=product_id)
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    context = {
+        'product': product,
+    }
 
-    request.session['bag'] = bag
-    print(request.session['bag'])
-    return redirect(redirect_url)
+    return render(request, 'products/product_detail.html', context)
